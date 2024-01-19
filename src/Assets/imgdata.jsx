aa09709ba4bf54114686fe33d67ea848e8a9da87
Mr.Link <108998528+MrKarimov01@users.noreply.github.com>
2024-01-20 03:17:09 +0500

    images get server

---
+++ src/Assets/imgdata.jsx
@@ -0,0 +1,25 @@
+export const imgData = {
+    logo:"https://i.ibb.co/rpckGQS/logo.png",
+    bg_img: "https://i.ibb.co/bQdjvpg/background.png",
+    black_shadow: "https://i.ibb.co/k45XrFT/black-shadow.png",
+    sun_rays: "https://i.ibb.co/R9F4bJy/sun-rays.png",
+    fog_1:"https://i.ibb.co/gS03LCQ/fog-1.png",
+    fog_2:"https://i.ibb.co/nQ2ZKfF/fog-2.png",
+    fog_3:"https://i.ibb.co/GfwKzcJ/fog-3.png",
+    fog_4:"https://i.ibb.co/cyNQ0LH/fog-4.png",
+    fog_5:"https://i.ibb.co/L630Qc0/fog-5.png",
+    fog_6:"https://i.ibb.co/MCvYh44/fog-6.png",
+    fog_7:"https://i.ibb.co/FXHkfF5/fog-7.png",
+    mountain_1:"https://i.ibb.co/94tCtXp/mountain-1.png",
+    mountain_2:"https://i.ibb.co/4tFJyb1/mountain-2.png",
+    mountain_3:"https://i.ibb.co/yF13CcY/mountain-3.png",
+    mountain_4:"https://i.ibb.co/LxY8vxf/mountain-4.png",
+    mountain_5:"https://i.ibb.co/cN4RdSv/mountain-5.png",
+    mountain_6:"https://i.ibb.co/VMYygpx/mountain-6.png",
+    mountain_7:"https://i.ibb.co/XVyF3z8/mountain-7.png",
+    mountain_8:"https://i.ibb.co/8KfS9tB/mountain-8.png",
+    mountain_9:"https://i.ibb.co/vzMjVJn/mountain-9.png",
+    mountain_10:"https://i.ibb.co/k9S235Y/mountain-10.png",
+
+
+}
